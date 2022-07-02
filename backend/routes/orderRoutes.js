9b5c2c5502dc26ece0613c75dfafbb3add33aebb
Daniel Eduardo Campos <daniele.campos@ingenico.com>
2022-07-01 19:43:18 -0500

    implement Delete Order function

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -139,4 +139,19 @@ orderRouter.post('/', isAuth, expressAsyncHandler(async (req, res) => {
     res.status(201).send({ message: 'New Order Created', order })
 }))
 
+orderRouter.delete(
+    '/:id',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            await order.remove();
+            res.send({ message: 'Order Deleted' });
+        } else {
+            res.status(404).send({ message: 'Order Not Found' });
+        }
+    })
+);
+
 export default orderRouter;
