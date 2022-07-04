4b78079a39a1091108e9ba34023d46df536ec4d9
Daniel Eduardo Campos <daniele.campos@ingenico.com>
2022-07-04 14:02:48 -0500

    implement Email Order send it after pay it

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Order from '../models/orderModel.js';
-import { isAuth, isAdmin } from '../utils.js';
+import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from '../utils.js';
 import expressAsyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
@@ -103,7 +103,10 @@ orderRouter.put(
     '/:id/pay',
     isAuth,
     expressAsyncHandler(async (req, res) => {
-        const order = await Order.findById(req.params.id)
+        const order = await Order.findById(req.params.id).populate(
+            'user',
+            'email name'
+        );
         if (order) {
             order.isPaid = true
             order.paidAt = Date.now()
@@ -114,6 +117,23 @@ orderRouter.put(
                 email_address: req.body.email_address
             }
             const updatedOrder = await order.save();
+            mailgun()
+                .messages()
+                .send(
+                    {
+                        from: 'Amazona <amazona@mg.yourdomain.com>',
+                        to: `${order.user.name} <${order.user.email}>`,
+                        subject: `New order ${order._id}`,
+                        html: payOrderEmailTemplate(order),
+                    },
+                    (error, body) => {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log(body);
+                        }
+                    }
+                );
             res.send({ message: 'Order Paid', order: updatedOrder })
         } else {
             res.status(404).send({ message: 'Order Not Found' });
